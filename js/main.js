12dfc8a19bbe83a58796945a5a9f35be8ae040ca
Abdulkaderadams <abdulkaderadams1@gmail.com>
2024-06-16 22:57:19 +0300

    second publish

--- js/main.js
+++ js/main.js
@@ -87,21 +87,26 @@ let products = {
       productName: "Salmon Sushi",
       description:
         "Rice, Carrot, Rocca, Crab, Salmon, Mango, Avocado, Edamame.",
+
+      nutri: ["39", "932"],
       category: "seafood",
       price: "9.5",
       image: "./images/JPEG-Salad/Salmon-sushi.jpg",
     },
     {
       productName: "Salmon Bowl",
-      category: "seafood",
-      price: "9.00",
       description:
         "Iceberg, Rocca, Carrot, Crab, Salmon, Mango, Avocado, Edamame.",
+      nutri: ["37", "752"],
+      category: "seafood",
+      price: "9.00",
+
       image: "./images/JPEG-Salad/Salmon-bowl.jpg",
     },
     {
       productName: "Crispy Shrimp",
       description: "Rice, Rocca, Crab, Fried-SHrimps, Mango, Coriander.",
+      nutri: ["32", "550"],
       category: "seafood",
       price: "8.5",
       image: "./images/JPEG-Salad/shrimp.jpg",
@@ -110,6 +115,7 @@ let products = {
     {
       productName: "Slay's Prawn",
       description: "Rocca, Cherry-Tomato, Avocado, Fetta Cheese, Shrimps.",
+      nutri: ["23", "370"],
       category: "seafood",
       price: "8.00",
       image: "./images/JPEG-Salad/Slay.jpg",
@@ -118,6 +124,7 @@ let products = {
     {
       productName: "Octo Bowl",
       description: "Iceberg, Rocca, Pomegranate, Quinoa, Octopus.",
+      nutri: ["26", "365"],
       category: "seafood",
       price: "8.00",
       image: "./images/JPEG-Salad/Octopus.jpg",
@@ -125,6 +132,7 @@ let products = {
     {
       productName: "Crab Sushi",
       description: "Rice, Carrot, Rocca, Crab, Mango, Avocado.",
+      nutri: ["28", "705"],
       category: "seafood",
       price: "6.00",
       image: "./images/JPEG-Salad/Crab-sushi.jpg",
@@ -132,6 +140,7 @@ let products = {
     {
       productName: "Oh Crab!",
       description: "Iceberg, Carrot, Cucumber, Crab, Avocado, Orange.",
+      nutri: ["20", "425"],
       category: "seafood",
       price: "5.00",
       image: "./images/JPEG-Salad/Oh-crab.jpg",
@@ -141,6 +150,7 @@ let products = {
       productName: "C'zar",
       description:
         "Iceberg, Cherry-Tomato, Grilled-Chicken, Cruton, Parmesan-Cheese.",
+      nutri: ["47", "555"],
       category: "chicken",
       price: "5.5",
       image: "./images/JPEG-Salad/Czar.jpg",
@@ -149,6 +159,7 @@ let products = {
       productName: "Goal Salad",
       description:
         "Iceberg, Rocca, Cherry-Tomato, Quinoa, Corn, Chicken, Pumpkin-Seeds.",
+      nutri: ["36", "583"],
       category: "chicken",
       price: "7.5",
       image: "./images/JPEG-Salad/Goal-Salad.jpg",
@@ -157,6 +168,7 @@ let products = {
       productName: "Buffy-Chicken",
       description:
         "Iceberg, Rocca, Corn, Cherry-Tomato, Dynamite-Chicken, Chips, Avocado.",
+      nutri: ["37", "685"],
       category: "chicken",
       price: "6.5",
       image: "./images/JPEG-Salad/Buffy-chick.jpg",
@@ -164,6 +176,7 @@ let products = {
     {
       productName: "Pop-Chicken",
       description: "Iceberg, Rocca, Corn, Cherry-Tomato, Fried-Pop-Chicken.",
+      nutri: ["22", "550"],
       category: "chicken",
       price: "5.5",
       image: "./images/JPEG-Salad/Popchicken.jpg",
@@ -172,6 +185,7 @@ let products = {
       productName: "Beef It Up!",
       description:
         "Iceberg, Rocca, Corn, Cherry-Tomato, Pomegranate, Red-Onion, Grilled-Beef.",
+      nutri: ["40", "622"],
       category: "meat",
       price: "7.5",
       image: "./images/JPEG-Salad/beefitup.jpg",
@@ -181,6 +195,7 @@ let products = {
       productName: "Say cheese :)",
       description:
         "Rocca, Bulgarian-Cheese, Strawberry, Green-Apple, Avocado, Roasted-Almond.",
+      nutri: ["15", "365"],
       category: "cheese",
       price: "5.5",
       image: "./images/JPEG-Salad/saycheese.jpg",
@@ -189,6 +204,7 @@ let products = {
       productName: "Rocka N Roll",
       description:
         "Rocca, Cherry-Tomato, Pomegranate, Raisins, Grilled-Halloumi, Walnuts.",
+      nutri: ["8", "445"],
       category: "cheese",
       price: "5.00",
       image: "./images/JPEG-Salad/Rocknroll.jpg",
@@ -197,6 +213,7 @@ let products = {
       productName: "Greek salad",
       description:
         "Iceberg, Rocca, Cherry-Tomato, Cucumber, Black-Olive, Pepper, Corn, Fetta-Cheese.",
+      nutri: ["26", "537"],
       category: "cheese",
       price: "4.5",
       image: "./images/JPEG-Salad/Greek-Salad.jpg",
@@ -206,6 +223,7 @@ let products = {
       productName: "Lentil Salad",
       description:
         "Iceberg, Rocca, Lentil, Cherry-Tomato, Broccoli, Beetroots, Coriander.",
+      nutri: ["13", "213"],
       category: "grain",
       price: "5.5",
       image: "./images/JPEG-Salad/Lentil.jpg",
@@ -214,6 +232,7 @@ let products = {
       productName: "Queen waa",
       description:
         "Rocca, Cherry-Tomato, Quinoa, Pomegranate, Red-Kidney, Walnuts.",
+      nutri: ["10", "375"],
       category: "grain",
       price: "4.5",
       image: "./images/JPEG-Salad/queenwaa.jpg",
@@ -222,6 +241,7 @@ let products = {
       productName: "ChickPeas",
       description:
         "Iceberg, Rocca, Cherry-Tomato, Pomegranate, Red-Onion, Fetta-Cheese, Chickpeas, Roasted-Almond.",
+      nutri: ["27", "580"],
       category: "grain",
       price: "5.00",
       image: "./images/JPEG-Salad/Chickpeas.jpg",
@@ -230,6 +250,7 @@ let products = {
       productName: "Joy Salad",
       description:
         "Fava-Beans, Red-Kidney, Cherry-Tomato, Coriander, Cucumber, Green-Onion, Avocado.",
+      nutri: ["57", "640"],
       category: "grain",
       price: "4.5",
       image: "./images/JPEG-Salad/JoySalad.jpg",
@@ -238,6 +259,7 @@ let products = {
       productName: "Let's Falafel",
       description:
         "Iceberg, Rocca, Cherry-Tomato, Pickles, Mint, Radish, Fried-Falafel.",
+      nutri: ["21", "380"],
       category: "grain",
       price: "5.00",
       image: "./images/JPEG-Salad/Falafel.jpg",
@@ -248,6 +270,7 @@ let products = {
       productName: "Turkey & Cheese",
       description:
         "Kashkaval, Smoked-Turkey, Mayo-Mustard, Iceberg, Cherry-Tomato, Cornichone.",
+      nutri: ["27", "420"],
       category: "sandwiches",
       price: "4.5",
       image: "./images/JPEG-Subs/Turkeyncheese.jpg",
@@ -255,6 +278,7 @@ let products = {
     {
       productName: "Pesto Halloumi",
       description: "Pesto spread, Grilled-Halloumi, Grilled-Tomato, Rocca.",
+      nutri: ["15", "425"],
       category: "sandwiches",
       price: "4.00",
       image: "./images/JPEG-Subs/Pesto.jpg",
@@ -262,12 +286,14 @@ let products = {
     {
       productName: "Scrambled Eggs",
       description: "Fetta cheese, Scrambled Eggs, Avocado, Rocca.",
+      nutri: ["20", "475"],
       category: "sandwiches",
       price: "5.00",
       image: "./images/JPEG-Subs/Eggs.jpg",
     },
     {
       productName: "Fetta Fiesta",
+      nutri: ["8", "315"],
       description: "Fetta-Cheese, Cucumber, Cherry-Tomato, Corn, Iceberg.",
       category: "sandwiches",
       price: "3.00",
@@ -275,6 +301,7 @@ let products = {
     },
     {
       productName: "Veggie Labneh",
+      nutri: ["10", "275"],
       description: "Labneh, Cucumber, Cherry-Tomato, Olives, Rocca.",
       category: "sandwiches",
       price: "2.5",
@@ -283,6 +310,7 @@ let products = {
     {
       productName: "Tuna Tuengo",
       description: "Tuna, Mayo-Mustard sauce, Cornichone, Corn, Iceberg.",
+      nutri: ["18", "375"],
       category: "sandwiches",
       price: "5.00",
       image: "./images/JPEG-Subs/Tuna.jpg",
@@ -292,6 +320,7 @@ let products = {
       productName: "Pesto Pasta",
       description:
         "Fusili-Pasta, Carrots, Brocoli, Mushrooms, Green-Pepper, Corn, Onion, Parmesan-Cheese.",
+      nutri: ["23", "796"],
       category: "pastas",
       price: "8.00",
       image: "./images/JPEG-Pasta/Pesto.jpg",
@@ -300,6 +329,7 @@ let products = {
       productName: "Chicken Pasta",
       description:
         "Fusili-Pasta, Chicken, Yellow-Pepper, Coriander, Corn, Avocado, Cherry-Tomato.",
+      nutri: ["42", "753"],
       category: "pastas",
       price: "7.5",
       image: "./images/JPEG-Pasta/Chicken.jpg",
@@ -308,6 +338,7 @@ let products = {
       productName: "Tuna Pasta",
       description:
         "Fusili-Pasta, Tuna, Carrots, Black-Olives, Corn, Cornichone, Iceberg.",
+      nutri: ["29", "652"],
       category: "pastas",
       price: "7.00",
       image: "./images/JPEG-Pasta/Tuna.jpg",
@@ -316,6 +347,7 @@ let products = {
       productName: "Veggie Pasta",
       description:
         "Fusili-Pasta, Cherry-Tomato, Brocoli, Cucumber, Green-Pepper, Mushroom, Fetta-Cheese.",
+      nutri: ["33", "693"],
       category: "pastas",
       price: "6.00",
       image: "./images/JPEG-Pasta/Veggie.jpg",
@@ -327,6 +359,7 @@ let products = {
       productName: "Beef Burger",
       description:
         "Grilled-Beef, Cheddar-Cheese, Rocca, Tartar-sauce, Grilled (Onion,Tomatoe,Mushroom).",
+      nutri: ["28", "415"],
       category: "burgers",
       price: "5.00",
       image: "./images/JPEG-Subs/BeefBurger.jpg",
@@ -335,9 +368,46 @@ let products = {
       productName: "Chicken Burger",
       description:
         "Grilled-Chicken, Avocado Cheddar-Cheese, Rocca, Iceberg, BBQ-sauce, Fries-Stix.",
+      nutri: ["30", "530"],
       category: "burgers",
       price: "4.00",
       image: "./images/JPEG-Subs/ChickenBurger.jpg",
+    },
+
+    // Juiiiiiiiiiiiiiiiicessss
+
+    {
+      productName: "Green Boost",
+      description:
+        "Ginger, Lemon, Orange, Cucumber, Green-Apple, Basil, Honey.",
+      nutri: ["2.5", "185"],
+      category: "juices",
+      price: "4.5",
+      image: "./images/JPEG-Juice/Green.jpg",
+    },
+    {
+      productName: "Lemon Ginger",
+      description: "Ginger, Lemon, Orange, Honey.",
+      nutri: ["2", "159"],
+      category: "juices",
+      price: "3.5",
+      image: "./images/JPEG-Juice/Lemon.jpg",
+    },
+    {
+      productName: "Orange",
+      description: "Fresh Orange.",
+      nutri: ["2", "115"],
+      category: "juices",
+      price: "3.00",
+      image: "./images/JPEG-Juice/Orange.jpg",
+    },
+    {
+      productName: "Carrot",
+      description: "Fresh Carrots.",
+      nutri: ["2.2", "94"],
+      category: "juices",
+      price: "3.00",
+      image: "./images/JPEG-Juice/Carrot.jpg",
     },
   ],
 };
@@ -398,14 +468,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Product text
     let text = document.createElement("p");
+    let divSpan = document.createElement("div");
+    let nutriFact = document.createElement("span");
+
+    // Add classes and content
+    divSpan.classList.add("text-left", "mt-auto"); // Use "mt-auto" to push divSpan to the bottom
+    nutriFact.classList.add("badge", "badge-success");
     text.classList.add("card-text", "text-left");
+
+    nutriFact.innerText =
+      "P : " + i.nutri[0] + "g" + " " + "/" + " " + i.nutri[1] + "kcal";
     text.innerText = i.description; // Assuming there's a description property
+
+    // Append elements
     cardBody.appendChild(text);
+    divSpan.appendChild(nutriFact);
+    cardBody.appendChild(divSpan);
     card.appendChild(cardBody);
 
     // Price
     let price = document.createElement("div");
-    price.classList.add("card-footer", "text-center", "bg-light");
+    price.classList.add("card-footer", "text-right", "bg-light");
     let priceTxt = document.createElement("small");
     priceTxt.classList.add("text-muted");
     priceTxt.innerText = "$" + i.price;
@@ -420,6 +503,8 @@ document.addEventListener("DOMContentLoaded", function () {
       productsContainer = document.getElementById("pastas");
     } else if (i.category === "burgers") {
       productsContainer = document.getElementById("burgers");
+    } else if (i.category === "juices") {
+      productsContainer = document.getElementById("juices");
     } else {
       productsContainer = document.getElementById("products");
     }
